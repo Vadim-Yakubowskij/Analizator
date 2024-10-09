37767f8f92e0e4b1eaa41586e556e311228f5fda
Вадим Якубовский <151447813+Vadim-Yakubowskij@users.noreply.github.com>
2024-10-09 20:46:59 +0300

    start

---
+++ Analizator/Analizator/Dealer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analizator
+{
+    public class Dealer : Deck
+    {
+        private string[] dealershand = { "", "" };
+        private int dealershandprice;
+        private int dealersfirstcard;
+        public void Hand()
+        {
+            dealershand[0] = GiveCard();
+            dealershand[1] = GiveCard();
+
+            for (int i = 0; i < 2; i++)
+            {
+                int cardprice;
+                if (int.TryParse(dealershand[i], out cardprice))
+                {
+                    dealershandprice += cardprice;
+                }
+                else
+                {
+                    if (dealershand[i] != "a")
+                    {
+                        dealershandprice += 10;
+                    }
+                    else
+                    {
+                        if (dealershandprice == 11)
+                        {
+                            dealershandprice = 2;
+                        }
+                        else
+                        {
+                            dealershandprice += 11;
+                        }
+                    }
+                }
+                if (i == 0)
+                {
+                    dealersfirstcard = cardprice;
+                }
+            }
+        }
+        public void ShowDealersFirstCard()
+        {
+            Console.WriteLine(dealersfirstcard);
+        }
+        public void ShowDealersHandPrice()
+        {
+            Console.WriteLine(dealershandprice);
+        }
+        public int GetDealersHandPrice()
+        {
+            return dealershandprice;
+        }
+    }
+}
---
+++ Analizator/Analizator/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analizator
+{
+    internal class Program 
+    { 
+    static void Main(string[] args)
+        {
+          bool flag = true;
+          while (flag != false)
+          {
+            Console.WriteLine("Добро пожаловать в блек джек");
+                if (flag == true)
+                {
+                    Deck deck = new Deck();
+                        Player player = new Player();
+                        Dealer dealer = new Dealer();
+                        player.Hand();
+                        Console.Write("Ваша рука:");
+                        player.ShowPlaersHandPrice();
+                        dealer.Hand();
+                        Console.Write("1 карта дилера:");
+                        dealer.ShowDealersFirstCard();
+                        string flag1 = "";
+                        while (flag1 != "no" && player.GetPlaersHandPrice() <=21)
+                        {
+                            Console.WriteLine("Введите yes , если xотите добрать карту, и no ,если не хотите");
+                            flag1 = Console.ReadLine();
+                            if (flag1 == "yes")
+                            {
+                                player.AddCard();
+                                Console.Write("Ваша рука:");
+                                player.ShowPlaersHandPrice();
+                            }
+                        }
+                        if (player.GetPlaersHandPrice() > 21)
+                        {
+                            Console.WriteLine("Ваша рука:");
+                            player.ShowPlaersHandPrice();
+                            Console.Write("Рука дилера:");
+                            dealer.ShowDealersHandPrice();
+                            Console.WriteLine("Вы проиграли,ваша рука больше 21");
+
+                        }
+                        else if (player.GetPlaersHandPrice() < dealer.GetDealersHandPrice())
+                        {
+                            Console.WriteLine("Ваша рука:");
+                            player.ShowPlaersHandPrice();
+                            Console.Write("Рука дилера:");
+                            dealer.ShowDealersHandPrice();
+                            Console.WriteLine("Вы проиграли,рука дилера больше");
+                        }
+                        else if (player.GetPlaersHandPrice() > dealer.GetDealersHandPrice())
+                        {
+                            if (player.GetPlaersHandPrice() != 21)
+                        {
+
+                        }
+                            else
+                            Console.WriteLine("Ваша рука:");
+                            player.ShowPlaersHandPrice();
+                            Console.Write("Рука дилера:");
+                            dealer.ShowDealersHandPrice();
+                            Console.WriteLine("Вы Выиграли");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ваша рука:");
+                            player.ShowPlaersHandPrice();
+                            Console.Write("Рука дилера:");
+                            dealer.ShowDealersHandPrice();
+                            Console.WriteLine("Ничья");
+                        }
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                }
+            }
+        
+    }
+}
